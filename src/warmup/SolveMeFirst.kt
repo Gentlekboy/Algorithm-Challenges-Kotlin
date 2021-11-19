92c2a0e2803585c5d785fcba22696dc511f31514
gentlekboy <gentlekboy@gmail.com>
2021-11-19 12:48:14 +0100

    added solution to the solve me first algorithm

---
+++ src/warmup/SolveMeFirst.kt
@@ -0,0 +1,27 @@
+package warmup
+/*
+Complete the function solveMeFirst to compute the sum of two integers.
+Sample 1 Input:
+a = 2
+b = 3
+
+Sample 1 Output:
+5
+
+Sample 2 Input:
+a = 7
+b = 3
+
+Sample 2 Output:
+10
+ */
+
+fun main() {
+    val a = 3
+    val b = 11
+    solveMeFirst(a, b)
+}
+
+fun solveMeFirst(a: Int, b: Int): Int {
+    return a + b
+}
