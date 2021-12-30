2c9c6f359d9af5b559415878522ea64c6c463c8c
gentlekboy <gentlekboy@gmail.com>
2021-12-30 08:36:13 +0100

    solved the "Hamming Distance" challenge

---
+++ src/day12/HammingDistance.kt
@@ -0,0 +1,28 @@
+package day12
+/*
+The hamming distance between two strings of equal length is
+the number of positions at which the corresponding symbols are different.
+In more technical terms, it is a measure of the minimum number of changes
+required to turn one string into another.
+For example, the Hamming distance between:
+"karolin" and "kerstin" is 3.
+"kathrin" and "kerstin" is 4.
+0000 and 1111 is 4.
+
+https://www.linkedin.com/posts/activity-6882091340512014336-lckz
+ */
+
+fun hammingDistance(firstString: String, secondString: String): Int {
+    //Create a variable to store the hamming distance
+    var hammingDistance = 0
+
+    //Go through the string and check where characters are different
+    for (position in firstString.indices) {
+        if (firstString[position] != secondString[position]) {
+            hammingDistance++
+        }
+    }
+
+    //Return the hamming distance (number of times characters were different)
+    return hammingDistance
+}
